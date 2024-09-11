2bd2084f636dd89f14a95670082f820bb68e48dd
Kirill Kolesnikov <kolesnikovkb@gmail.com>
2024-09-11 13:47:12 +0300

    add alembic

--- app/employee/models.py
+++ app/employee/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 from sqlalchemy import String
 
 from uuid import UUID
-from datetime import date
+from datetime import datetime
 
 
 class Employee(Base):
@@ -15,5 +15,5 @@ class Employee(Base):
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
-    created_at: Mapped[date] = mapped_column(server_default=func.current_timestamp())
-    updated_at:Mapped[date] = mapped_column(server_default=func.current_timestamp())
+    created_at: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
+    updated_at:Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
--- app/organization/models.py
+++ app/organization/models.py
@@ -3,11 +3,11 @@ from app.database import Base
 
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy import String, Text, ForeignKey, Enum
 
 import enum
 from uuid import UUID
-from datetime import date
+from datetime import datetime
 
 
 class OrganizationTypeEnum(str, enum.Enum):
@@ -17,20 +17,20 @@ class OrganizationTypeEnum(str, enum.Enum):
 
 
 class Organization(Base):
-    __table__ = "organization"
+    __tablename__ = "organization"
 
     id: Mapped[UUID] = mapped_column(server_default=func.gen_random_uuid(), primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(Text())
-    type: Mapped[OrganizationTypeEnum]
-    created_at: Mapped[date] = mapped_column(server_default=func.current_timestamp())
-    updated_at:Mapped[date] = mapped_column(server_default=func.current_timestamp())
+    type: Mapped[OrganizationTypeEnum] = mapped_column(Enum(OrganizationTypeEnum), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
+    updated_at:Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
 
 
 class OrganizationResponsible(Base):
-    __table__ = "organization_responsible"
+    __tablename__ = "organization_responsible"
     
     id: Mapped[UUID] = mapped_column(server_default=func.gen_random_uuid(), primary_key=True)
     organization_id: Mapped[UUID] = mapped_column(ForeignKey("organization.id", ondelete="CASCADE"), nullable=False)
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("employee", ondelete="CASCADE"), nullable=False)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("employee.id", ondelete="CASCADE"), nullable=False)
 
