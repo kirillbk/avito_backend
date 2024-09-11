3e432b7d7c580d4e69cf7bda4d81923ab8295343
Kirill Kolesnikov <kolesnikovkb@gmail.com>
2024-09-11 04:29:51 +0300

    add DB

---
+++ app/database.py
@@ -0,0 +1,32 @@
+from app.config import settings
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from sqlalchemy import URL
+from sqlalchemy.orm import DeclarativeBase
+
+from typing import AsyncGenerator
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db_url = URL.create(
+    drivername="postgresql+asyncpg",
+    username=settings.postgres_username,
+    password=settings.postgres_password,
+    host=settings.postgres_host,
+    port=settings.postgres_port,
+    database=settings.postgres_database,
+)
+engine = create_async_engine(db_url, echo=settings.debug)
+session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with session_maker() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 from app.tenders.router import router as tenders_router
 from app.bids.router import router as bids_router
 from app.error import system_exception_handler, http_exception_handler, request_error_handler
+from app.database import engine
 
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 from fastapi.responses import PlainTextResponse
 
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Tender Management API")
+
+@asynccontextmanager
+async def lifespan(_):
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(title="Tender Management API", lifespan=lifespan)
+
 app.include_router(tenders_router, prefix="/api")
 app.include_router(bids_router, prefix="/api")
+
 app.add_exception_handler(Exception, system_exception_handler)
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, request_error_handler)
