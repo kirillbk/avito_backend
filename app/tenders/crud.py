fd82848b2d043df17be99b3105c8bb6feb661810
Kirill Kolesnikov <kolesnikovkb@gmail.com>
2024-09-12 22:31:48 +0300

    add /tenders/edit

--- app/tenders/crud.py
+++ app/tenders/crud.py
@@ -1,7 +1,8 @@
-from app.tenders.models import Tender, TenderInfo, TenderServiceTypeEnum, TenderVersion
+from app.tenders.models import Tender, TenderVersion, TenderServiceTypeEnum, TenderInfo
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from uuid import UUID
 from typing import Sequence
@@ -16,20 +17,22 @@ async def add_tender(
     serviceType: TenderServiceTypeEnum,
 ) -> Tender:
     async with db as session:
-        tender_info = TenderInfo(
+        tender_version = TenderVersion(
             name=name, description=description, serviceType=serviceType
         )
-        session.add(tender_info)
+        session.add(tender_version)
         await session.commit()
 
         tender = Tender(
-            organizationId=organizationId, creatorId=creatorId, _info=tender_info
+            organizationId=organizationId, creatorId=creatorId, _version=tender_version
         )
         session.add(tender)
         await session.commit()
 
-        tender_version = TenderVersion(tender_id=tender.id, tender_info_id=tender_info.id)
-        session.add(tender_version)
+        tender_info = TenderInfo(
+            tender_id=tender.id, tender_version_id=tender_version.id
+        )
+        session.add(tender_info)
         await session.commit()
 
     return tender
@@ -41,9 +44,9 @@ async def get_tenders_by_type(
     offset: int,
     service_type: TenderServiceTypeEnum | None = None,
 ) -> Sequence[Tender]:
-    stmt = select(Tender).join(TenderInfo)
+    stmt = select(Tender).join(TenderVersion).options(joinedload(Tender._version))
     if service_type:
-        stmt = stmt.where(TenderInfo.serviceType == service_type)
+        stmt = stmt.where(TenderVersion.serviceType == service_type)
     stmt = stmt.limit(limit).offset(offset)
 
     async with db as session:
@@ -53,7 +56,11 @@ async def get_tenders_by_type(
 async def get_tenders_by_user(
     db: AsyncSession, limit: int, offset: int, creator_id: UUID
 ) -> Sequence[Tender]:
-    stmt = select(Tender).where(Tender.creatorId == creator_id)
+    stmt = (
+        select(Tender)
+        .where(Tender.creatorId == creator_id)
+        .options(joinedload(Tender._version))
+    )
     stmt = stmt.limit(limit).offset(offset)
 
     async with db as session:
@@ -63,3 +70,33 @@ async def get_tenders_by_user(
 async def get_tender(db: AsyncSession, tender_id: UUID) -> Tender:
     async with db as session:
         return await session.get(Tender, tender_id)
+
+
+async def update_tender(
+    db: AsyncSession,
+    tender_id: UUID,
+    name: str | None,
+    description: str | None,
+    serviceType: TenderServiceTypeEnum | None
+) -> Tender:
+    async with db as session:
+        stmt = select(Tender).where(Tender.id == tender_id).with_for_update()
+        tender = await session.scalar(stmt)
+        old_tender_version = await session.get(TenderVersion, tender.version_id)
+        new_tender_version = TenderVersion(
+            name=name if name else old_tender_version.name,
+            description=description if description else old_tender_version.description,
+            serviceType=serviceType if serviceType else old_tender_version.serviceType,
+            version = old_tender_version.version + 1   
+        )
+        session.add(new_tender_version)
+        await session.commit()
+
+        tender._version = new_tender_version
+        await session.commit()
+
+        tender_info = TenderInfo(tender_id=tender_id, tender_version_id=new_tender_version.id)
+        session.add(tender_info)
+        await session.commit()
+
+    return tender
--- app/tenders/router.py
+++ app/tenders/router.py
@@ -8,11 +8,10 @@ from app.tenders.models import TenderServiceTypeEnum, TenderStatusEnum
 from app.database import get_db
 from app.employee.crud import get_user
 from app.organization.crud import get_responsible
-from app.tenders.crud import add_tender, get_tenders_by_type, get_tenders_by_user
+from app.tenders.crud import add_tender, get_tenders_by_type, get_tenders_by_user, get_tender, update_tender
 from app.error import ErrorResponse, ErrorResponseSchema
 
 from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from uuid import UUID
@@ -44,24 +43,24 @@ async def list_tenders(
     },
 )
 async def add_new_tender(
-    tender_info: NewTenderSchema, db: AsyncSession = Depends(get_db)
+    new_tender: NewTenderSchema, db: AsyncSession = Depends(get_db)
 ) -> TenderSchema:
-    user = await get_user(tender_info.creatorUsername, db)
+    user = await get_user(new_tender.creatorUsername, db)
     if not user:
         return ErrorResponse(
-            f"Пользователя {tender_info.creatorUsername} не существует",
+            f"Пользователя {new_tender.creatorUsername} не существует",
             status_code=status.HTTP_401_UNAUTHORIZED,
         )
 
-    responsible = await get_responsible(user.id, tender_info.organizationId, db)
+    responsible = await get_responsible(user.id, new_tender.organizationId, db)
     if not responsible:
         return ErrorResponse(
-            f"Пользователь {tender_info.creatorUsername} не является представителем организации {tender_info.organizationId}",
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            f"Пользователь {new_tender.creatorUsername} не является представителем организации {new_tender.organizationId}",
+            status_code=status.HTTP_403_FORBIDDEN,
         )
 
     return await add_tender(
-        db, user.id, **tender_info.model_dump(exclude={"creatorUsername"})
+        db, user.id, **new_tender.model_dump(exclude={"creatorUsername"})
     )
 
 
@@ -84,8 +83,15 @@ async def get_user_tenders(
     return await get_tenders_by_user(db, limit, offset, user.id)
 
 
-@router.get("/{tender_id}/status")
-async def get_tender_status(tender_id: UUID, username: str) -> TenderStatusEnum:
+@router.get(
+    "/{tender_id}/status",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": ErrorResponseSchema},
+        status.HTTP_403_FORBIDDEN: {"model": ErrorResponseSchema},
+        status.HTTP_404_NOT_FOUND: {"model": ErrorResponseSchema},
+    },
+)
+async def get_tender_status(tender_id: UUID, username: str,  db: AsyncSession = Depends(get_db)) -> TenderStatusEnum:
     pass
 
 
@@ -96,11 +102,39 @@ async def set_tender_status(
     pass
 
 
-@router.patch("/{tender_id}/edit")
+@router.patch(
+    "/{tender_id}/edit",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": ErrorResponseSchema},
+        status.HTTP_403_FORBIDDEN: {"model": ErrorResponseSchema},
+        status.HTTP_404_NOT_FOUND: {"model": ErrorResponseSchema},
+    },
+)
 async def edit_tender(
-    tender_id: UUID, username: str, tender_params: EditTenderSchema
+    tender_id: UUID, username: str, tender_info: EditTenderSchema, db: AsyncSession = Depends(get_db)
 ) -> TenderSchema:
-    pass
+    user = await get_user(username, db)
+    if not user:
+        return ErrorResponse(
+            f"Пользователя {username} не существует",
+            status_code=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    tender = await get_tender(db, tender_id)
+    if not tender:
+        return ErrorResponse(
+            f"Тендера {tender_id} не существует",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+
+    responsible = await get_responsible(user.id, tender.organizationId, db)
+    if not responsible:
+        return ErrorResponse(
+            f"Пользователь {username} не является представителем организации {tender.organizationId}",
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
+
+    return await update_tender(db, tender_id, **tender_info.model_dump())
 
 
 @router.put("/{tender_id}/rollback/{version}")
