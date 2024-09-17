7285a36521c6ea1115df032474dc41cc620da03a
Kirill Kolesnikov <kolesnikovkb@gmail.com>
2024-09-17 03:00:24 +0300

    test tender rollback

--- test/tenders/test_edit.py
+++ test/tenders/test_edit.py
@@ -7,7 +7,7 @@ import pytest
 
 class TestEdit:
     @pytest.mark.anyio
-    async def test_get_no_user(self, aclient: AsyncClient):
+    async def test_no_user(self, aclient: AsyncClient):
         response = await aclient.patch(
             "api/tenders/550e8400-e29b-41d4-a716-446655440000/edit",
             params={"username": "no_user"},
@@ -20,7 +20,7 @@ class TestEdit:
         check_error(response, status.HTTP_401_UNAUTHORIZED)
    
     @pytest.mark.anyio
-    async def test_get_no_tender(self, aclient: AsyncClient):
+    async def test_no_tender(self, aclient: AsyncClient):
         response = await aclient.patch(
             "api/tenders/550e8400-e29b-41d4-a716-446655440000/edit",
             params={"username": "user1"},
@@ -31,9 +31,27 @@ class TestEdit:
             }
         )
         check_error(response, status.HTTP_404_NOT_FOUND)
+
+    @pytest.mark.anyio
+    async def test_user_is_not_responsible(self, new_tender, aclient: AsyncClient):
+        new_tender["creatorUsername"] = "user1"
+        new_tender["organizationId"] = "550e8400-e29b-41d4-a716-446655440020"
+        response = await aclient.post("api/tenders/new", json=new_tender)
+        tender_id = response.json()["id"]
+
+        response = await aclient.patch(
+            f"api/tenders/{tender_id}/edit",
+            params={"username": "user30"},
+            json={
+                "name": "string",
+                "description": "string",
+                "serviceType": "Construction",
+            },
+        )
+        check_error(response, status.HTTP_403_FORBIDDEN)
    
     @pytest.mark.anyio
-    async def test_get_user_is_not_responsible(self, new_tender: dict[str, str], aclient: AsyncClient):
+    async def test_edit(self, new_tender: dict[str, str], aclient: AsyncClient):
         new_tender["creatorUsername"] = "user1"
         new_tender["organizationId"] = "550e8400-e29b-41d4-a716-446655440020"
         response = await aclient.post("api/tenders/new", json=new_tender)
--- test/tenders/test_my.py
+++ test/tenders/test_my.py
@@ -13,17 +13,21 @@ class TestMy:
 
     @pytest.mark.anyio
     async def test_get(self, new_tender: dict[str, str], aclient: AsyncClient):
-        new_tender["creatorUsername"] = "user1"
         new_tender[ "organizationId"] = "550e8400-e29b-41d4-a716-446655440020"
+        new_tender["creatorUsername"] = "user1"
         new_tender["name"] = "aaa"
         await aclient.post("api/tenders/new", json=new_tender)
         new_tender["name"] = "zzz"
         await aclient.post("api/tenders/new", json=new_tender)
-        new_tender["creatorUsername"] = "user2"
         new_tender["name"] = "bbb"
         await aclient.post("api/tenders/new", json=new_tender)
-        response = await aclient.get("api/tenders/my", params={"username": "user1"})
+        new_tender["creatorUsername"] = "user2"
+        new_tender["name"] = "yyy"
+        await aclient.post("api/tenders/new", json=new_tender)
 
+        response = await aclient.get("api/tenders/my", params={"username": "user1"})
         assert response.status_code == status.HTTP_200_OK
         tenders = response.json()
-        assert len(tenders) == 2
+        assert all(tenders[i]["name"] <= tenders[i + 1]["name"] for i in range(len(tenders) - 1))
+        assert len(tenders) == 3
+
